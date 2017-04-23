2367f6d3f63fb19c08441049fbe01baae7931795
SamiWiz <ss9td@mail.umkc.edu>
2017-04-22 20:30:02 -0500

    sure

--- android/app/src/main/java/com/upstart/MainApplication.java
+++ android/app/src/main/java/com/upstart/MainApplication.java
@@ -1,20 +1,14 @@
 package com.upstart;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import cl.json.RNSharePackage;
-import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.github.wuxudong.rncharts.MPAndroidChartPackage;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.github.wuxudong.rncharts.MPAndroidChartPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,14 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNSharePackage(),
-            new ReactNativeOneSignalPackage(),
-            new ReactMaterialKitPackage(),
-            new ImagePickerPackage(),
-            new ReactNativeConfigPackage(),
-            new MPAndroidChartPackage()
+          new MainReactPackage()
       );
     }
   };
